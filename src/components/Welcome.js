333235e7c0a62a63c05a6fe1b9156e743efe70e4
brennen <bmar566@gmail.com>
2020-05-07 15:33:07 -0500

    added logout button and intro container

---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React from "react";
+export default () => {
+  return (
+    <div className="intro">
+      Welcome to Let's Fly. Let's Fly makes finding pilots to fly with much
+      easier. You can look through the flights to find one you may be interested
+      in and send the poster a message or you can post a flight yourself. You
+      can also search for pilots you already know and send them a direct
+      message. Fly safe and remember to keep the blue side up! Happy Flying.
+    </div>
+  );
+};
