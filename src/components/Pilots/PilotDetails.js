a5872d665a179ab749ac173c8ffcd189d9204bcb
brennen <bmar566@gmail.com>
2020-05-18 10:14:23 -0500

    added if else statments to flight list and pilot details so you cant send yourself a msg

--- src/components/Pilots/PilotDetails.js
+++ src/components/Pilots/PilotDetails.js
@@ -8,69 +8,112 @@ export default ({ pilot }) => {
 
   const [selectedPilot, setPilot] = useState({ pilot: {} });
 
-  return (
-    <section className="pilot_details">
-      <img
-        src={pilot.image}
-        style={{ width: "100px" }}
-        alt="profile pic"
-        className="pilotPic"
-      />
-      <div></div>
-      <h3 className="pilot_name">{pilot.name}</h3>
-      <div className="pilot_username">
-        <b>Username:</b> {pilot.username}
-      </div>
-      <div className="pilot_email">
-        <b>Email:</b> {pilot.email}
-      </div>
-      <div className="pilot_cert">
-        <b>Certificate Held:</b> {pilot.certificate}
-      </div>
-      <div className="pilot_endorsements">
-        <b>Endorsements:</b> {pilot.endorsements}
-      </div>
+  const userId = parseInt(localStorage.getItem("letsFly_user"));
 
-      <div className="pilot_hours">
-        <b>Flight Hours:</b> {pilot.hours}
-      </div>
-      <div className="pilot_base">
-        <b>Home Base Airport:</b> {pilot.baseApt}
-      </div>
-      <div className="pilot_plane">
-        <b>Plane Owned:</b>
-        {pilot.planeType}
-      </div>
-      <div className="pilot_ifr">
-        <b> Instrument Rated:</b> {pilot.ifr === "true" ? "YES" : "NO"}
-      </div>
-      <Button
-        size="lg"
-        color="primary"
-        onClick={() => {
-          setPilot({ pilot });
-          toggleReply();
-        }}
-      >
-        <b>Send Message</b>
-      </Button>
+  if (userId === pilot.id) {
+    return (
+      <section className="pilot_details">
+        <img
+          src={pilot.image}
+          style={{ width: "100px" }}
+          alt="profile pic"
+          className="pilotPic"
+        />
+        <div></div>
+        <h3 className="pilot_name">{pilot.name}</h3>
+        <div className="pilot_username">
+          <b>Username:</b> {pilot.username}
+        </div>
+        <div className="pilot_email">
+          <b>Email:</b> {pilot.email}
+        </div>
+        <div className="pilot_cert">
+          <b>Certificate Held:</b> {pilot.certificate}
+        </div>
+        <div className="pilot_endorsements">
+          <b>Endorsements:</b> {pilot.endorsements}
+        </div>
 
-      <Modal isOpen={replyModal} toggle={toggleReply}>
-        <ModalHeader toggle={toggleReply}>Send Message</ModalHeader>
-        <ModalBody>
-          <MessageForm
-            key={selectedPilot.pilot.id}
-            toggleReply={toggleReply}
-            {...selectedPilot}
-            pilot={pilot}
-          />
-        </ModalBody>
-        <ModalFooter>
-          <Button color="secondary" onClick={toggleReply}>
-            <b>Cancel</b>
-          </Button>
-        </ModalFooter>
-      </Modal>
-    </section>
-  );
+        <div className="pilot_hours">
+          <b>Flight Hours:</b> {pilot.hours}
+        </div>
+        <div className="pilot_base">
+          <b>Home Base Airport:</b> {pilot.baseApt}
+        </div>
+        <div className="pilot_plane">
+          <b>Plane Owned:</b>
+          {pilot.planeType}
+        </div>
+        <div className="pilot_ifr">
+          <b> Instrument Rated:</b> {pilot.ifr === "true" ? "YES" : "NO"}
+        </div>
+      </section>
+    );
+  } else {
+    return (
+      <section className="pilot_details">
+        <img
+          src={pilot.image}
+          style={{ width: "100px" }}
+          alt="profile pic"
+          className="pilotPic"
+        />
+        <div></div>
+        <h3 className="pilot_name">{pilot.name}</h3>
+        <div className="pilot_username">
+          <b>Username:</b> {pilot.username}
+        </div>
+        <div className="pilot_email">
+          <b>Email:</b> {pilot.email}
+        </div>
+        <div className="pilot_cert">
+          <b>Certificate Held:</b> {pilot.certificate}
+        </div>
+        <div className="pilot_endorsements">
+          <b>Endorsements:</b> {pilot.endorsements}
+        </div>
+
+        <div className="pilot_hours">
+          <b>Flight Hours:</b> {pilot.hours}
+        </div>
+        <div className="pilot_base">
+          <b>Home Base Airport:</b> {pilot.baseApt}
+        </div>
+        <div className="pilot_plane">
+          <b>Plane Owned:</b>
+          {pilot.planeType}
+        </div>
+        <div className="pilot_ifr">
+          <b> Instrument Rated:</b> {pilot.ifr === "true" ? "YES" : "NO"}
+        </div>
+        <Button
+          size="lg"
+          color="primary"
+          onClick={() => {
+            setPilot({ pilot });
+            toggleReply();
+          }}
+        >
+          <b>Send Message</b>
+        </Button>
+
+        <Modal isOpen={replyModal} toggle={toggleReply}>
+          <ModalHeader toggle={toggleReply}>Send Message</ModalHeader>
+          <ModalBody>
+            <MessageForm
+              key={selectedPilot.pilot.id}
+              toggleReply={toggleReply}
+              {...selectedPilot}
+              pilot={pilot}
+            />
+          </ModalBody>
+          <ModalFooter>
+            <Button color="secondary" onClick={toggleReply}>
+              <b>Cancel</b>
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </section>
+    );
+  }
 };
